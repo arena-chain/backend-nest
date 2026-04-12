1ce7bb197f4ba81d256de9d3cdd42d4e9861070a
Khalil Mouscou <Mouscou.Mohamedkhalil@esprit.tn>
2026-04-12 16:45:48 +0100

    update back

--- src/common/utils/file-upload.utils.ts
+++ src/common/utils/file-upload.utils.ts
@@ -5,7 +5,7 @@ import { mkdirSync } from 'fs';
 
 export const imageUploadOptions = {
     fileFilter: (req: any, file: any, cb: any) => {
-        if (file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
+        if (file.mimetype.match(/\/(jpg|jpeg|png|gif|webp)$/)) {
             cb(null, true);
         } else {
             cb(new HttpException(`Unsupported file type ${extname(file.originalname)}`, HttpStatus.BAD_REQUEST), false);
